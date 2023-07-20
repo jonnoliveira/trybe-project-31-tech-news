caf33c59769b1fbf81bfa01ad7e656737a55d638
jonnoliveira <jonnutri@gmail.com>
2023-07-20 10:11:45 -0300

    feat: added search_by_title implementation - req07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,21 @@
+from tech_news import database
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # https://www.mongodb.com/docs/v6.0/reference/operator/query/regex/
+    news_list = database.search_news(
+        {"title": {"$regex": title, "$options": "i"}}
+    )
+    result = []
+
+    if news_list:
+        for new in news_list:
+            item = f"{new['title']}", f"{new['url']}"
+            result.append(item)
+        return result
+
+    return news_list
 
 
 # Requisito 8
