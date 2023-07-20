80e441d23a9a03478080a714a6645bc4b9201762
jonnoliveira <jonnutri@gmail.com>
2023-07-20 11:12:07 -0300

    feat: added search_by_date implementation - req08

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,22 @@
 from tech_news import database
+import datetime
+
+
+def iterating_over_the_data(news_list):
+    result = []
+
+    for new in news_list:
+        item = f"{new['title']}", f"{new['url']}"
+        result.append(item)
+    return result
+
+
+# https://stackoverflow.com/questions/16870663/how-do-i-validate-a-date-string-format-in-python
+def validate_date(date):
+    try:
+        datetime.datetime.fromisoformat(date)
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 7
@@ -7,21 +25,30 @@ def search_by_title(title):
     news_list = database.search_news(
         {"title": {"$regex": title, "$options": "i"}}
     )
-    result = []
 
     if news_list:
-        for new in news_list:
-            item = f"{new['title']}", f"{new['url']}"
-            result.append(item)
-        return result
+        return iterating_over_the_data(news_list)
 
     return news_list
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    validate_date(date)
+
+    # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+    date_correction = datetime.datetime.strptime(date, "%Y-%m-%d").strftime(
+        "%d/%m/%Y"
+    )
+
+    news_list = database.search_news(
+        {"timestamp": {"$regex": date_correction}}
+    )
+
+    if news_list:
+        return iterating_over_the_data(news_list)
+
+    return news_list
 
 
 # Requisito 9
