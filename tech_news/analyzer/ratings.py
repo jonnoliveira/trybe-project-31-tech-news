d9494e10da9e0e3d94a6d941aaea071837f5dcbc
jonnoliveira <jonnutri@gmail.com>
2023-07-20 12:31:02 -0300

    fix: redundancy - req10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -2,29 +2,21 @@ from tech_news import database
 
 
 def search_and_count_categories(data_news):
-    categories = []
     categories__count = {}
 
     for new in data_news:
-        categories.append(new["category"])
         if new["category"] not in categories__count:
             categories__count[new["category"]] = 1
         else:
             categories__count[new["category"]] += 1
 
-    return {"categories": categories, "count": categories__count}
+    return categories__count
 
 
 # Requisito 10
 def top_5_categories():
     data_news = database.find_news()
-    searched = search_and_count_categories(data_news)
-
-    categories = searched["categories"]
-    categories_count = searched["count"]
-
-    if len(categories) < 5:
-        return categories
+    categories_count = search_and_count_categories(data_news)
 
     # https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
     # https://stackoverflow.com/questions/24728933/sort-dictionary-alphabetically-when-the-key-is-a-string-name
